bbafceaec8876ee8eb6d16d47499d5a7f702c460
tarek shno <mohammad.sh12121@gmail.com>
2025-09-19 15:18:36 +0300

    fixed cors error

--- config/whiteList.js
+++ config/whiteList.js
@@ -2,6 +2,8 @@ const whitelist = [
   `https://www.yoursite.com`,
   "http://127.0.0.1:5500",
   "https://localhost:3500",
+  "http://localhost:5173/",
+  "https://nodejs-tut-1-1syc.onrender.com",
 ];
 
 module.exports = whitelist;
--- middleware/credentials.js
+++ middleware/credentials.js
@@ -2,10 +2,10 @@ const whitelist = require("../config/whiteList.js");
 
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
-  if (whitelist.indexOf(origin)) {
+  if (whitelist.includes(origin)) {
+    // Use includes() instead
     res.header("Access-Control-Allow-Credentials", true);
   }
   next();
 };
-
 module.exports = credentials;
